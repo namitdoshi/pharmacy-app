b0e5ca651a99a2883ecccd0f5a405e5cd01406c7
pixy1024 <pixy1024@noreply.github.com>
2021-05-03 17:41:14 +0530

    feignclient implemented

---
+++ medical-representative-schedule-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into medical_representatives (name, phone_number) values('Medical Representative 1', '9898780001');
+insert into medical_representatives (name, phone_number) values('Medical Representative 2', '9898780002');
+insert into medical_representatives (name, phone_number) values('Medical Representative 3', '9898780003');
+insert into medical_representatives (name, phone_number) values('Medical Representative 4', '9898780004');
+insert into medical_representatives (name, phone_number) values('Medical Representative 5', '9898780005');
---
+++ medical-representative-schedule-service/src/test/java/com/cognizant/medicalrepresentativeschedule/MedicalRepresentativeScheduleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.medicalrepresentativeschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRepresentativeScheduleService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
