2866ee88d489067acdf20463f7343897b459b4d7
Namit Doshi <namitdoshi90@gmail.com>
2021-05-03 19:18:56 +0530

    integrated stock microservice

---
+++ pharmacysupply/src/main/java/com/cognizant/pharmacysupply/feignclient/MedicineStockFeignClient.java
@@ -0,0 +1,18 @@
+package com.cognizant.pharmacysupply.feignclient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import com.cognizant.pharmacysupply.model.MedicineStock;
+
+@FeignClient(url = "localhost:8081", name = "medicine-stock-service")
+public interface MedicineStockFeignClient {
+	
+	@PostMapping("/api/medicine-stock/get-stock-count/{medicine}")
+	public MedicineStock getNumberOfTabletsInStockByName(@RequestHeader(name = "Authorization") String token,@PathVariable("medicine") String medicine);
+	
+	@PostMapping("/api/medicine-stock/update-stock/{medicine}/{count}")
+	public Boolean updateNumberOfTabletsInStockByName(@RequestHeader(name = "Authorization") String token,@PathVariable("medicine") String medicine, @PathVariable("count") int count);	
+}
--- pharmacysupply/src/main/java/com/cognizant/pharmacysupply/service/PharmacyServiceImpl.java
+++ pharmacysupply/src/main/java/com/cognizant/pharmacysupply/service/PharmacyServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.cognizant.pharmacysupply.exception.MedicineNotFoundException;
 import com.cognizant.pharmacysupply.exception.TokenValidationFailedException;
 import com.cognizant.pharmacysupply.feignclient.AuthFeignClient;
+import com.cognizant.pharmacysupply.feignclient.MedicineStockFeignClient;
 import com.cognizant.pharmacysupply.model.JwtResponse;
 import com.cognizant.pharmacysupply.model.MedicineDemand;
 import com.cognizant.pharmacysupply.model.MedicineStock;
@@ -16,6 +17,7 @@ import com.cognizant.pharmacysupply.model.PharmacyMedicineSupply;
 import com.cognizant.pharmacysupply.repository.MedicineDemandRepository;
 import com.cognizant.pharmacysupply.repository.PharmacyMedicineSupplyRepository;
 
+import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -33,6 +35,9 @@ public class PharmacyServiceImpl implements PharmacyService {
 	@Autowired
 	private AuthFeignClient authFeign;
 
+	@Autowired
+	private MedicineStockFeignClient stockFeignClient; 
+
 	@Override
 	public List<PharmacyMedicineSupply> getPharmacySupplyCount(String token, List<MedicineDemand> medicineDemandList)
 			throws MedicineNotFoundException {
@@ -42,15 +47,17 @@ public class PharmacyServiceImpl implements PharmacyService {
 
 		for (MedicineDemand medicineDemand : medicineDemandList) {
 			PharmacyMedicineSupply pharmacyMedicineSupply = new PharmacyMedicineSupply();
-			MedicineStock medicineStock = getNumberOfTablets(medicineDemand);
+			MedicineStock medicineStock = getNumberOfTablets(token, medicineDemand);
+			log.info("***********" + medicineDemand);
 			log.info("{}", medicineStock);
-			log.info("Medicine {} , Tablets {}", medicineDemand.getMedicineName(), medicineStock.getTabletsInStock());
+			log.info("Medicine {} , Tablets {}", medicineDemand.getMedicineName(),
+					medicineStock.getNumberOfTabletsInStock());
 
-			if (medicineStock.getTabletsInStock() < medicineDemand.getDemandCount()) {
+			if (medicineStock.getNumberOfTabletsInStock() < medicineDemand.getDemandCount()) {
 				return null;
 			}
 
-			setSupply(pharmacyMedicineSupply, medicineDemand, medicineStock);
+			setSupply(token, pharmacyMedicineSupply, medicineDemand, medicineStock);
 			list.add(pharmacyMedicineSupply);
 		}
 		pharmacyMedicineSupplyRepository.saveAll(list);
@@ -59,21 +66,25 @@ public class PharmacyServiceImpl implements PharmacyService {
 		return list;
 	}
 
-	public void setSupply(PharmacyMedicineSupply medicineSupply, MedicineDemand medicineDemand,
+	public void setSupply(String token, PharmacyMedicineSupply medicineSupply, MedicineDemand medicineDemand,
 			MedicineStock medicineStock) throws MedicineNotFoundException {
 		log.info("Start");
 		int val = 0;
-		log.info("number of tablets {}", medicineStock.getTabletsInStock());
-		if (medicineStock.getTabletsInStock() < medicineDemand.getDemandCount()) {
-			medicineSupply.setSupplyCount(medicineStock.getTabletsInStock());
+		log.info("number of tablets {}", medicineStock.getNumberOfTabletsInStock());
+		if (medicineStock.getNumberOfTabletsInStock() < medicineDemand.getDemandCount()) {
+			medicineSupply.setSupplyCount(medicineStock.getNumberOfTabletsInStock());
 
 		} else {
 			medicineSupply.setSupplyCount(medicineDemand.getDemandCount());
-			val = medicineStock.getTabletsInStock() - medicineDemand.getDemandCount();
+			val = medicineStock.getNumberOfTabletsInStock() - medicineDemand.getDemandCount();
 
 		}
 		log.info("val = {}", val);
-
+		try {
+			stockFeignClient.updateNumberOfTabletsInStockByName(token, medicineDemand.getMedicineName(), val);
+		} catch (FeignException ex) {
+			throw new MedicineNotFoundException("Medicine not found");
+		}
 		medicineSupply.setMedicineName(medicineDemand.getMedicineName());
 		log.info("medicineDemand {} medicineSupply {}", medicineDemand, medicineSupply);
 		medicineSupply.setPharmacyName(medicineStock.getPharmacyName());
@@ -81,11 +92,17 @@ public class PharmacyServiceImpl implements PharmacyService {
 		log.info("End");
 	}
 
-	public MedicineStock getNumberOfTablets(MedicineDemand medicineDemand) throws MedicineNotFoundException {
+	public MedicineStock getNumberOfTablets(String token, MedicineDemand medicineDemand)
+			throws MedicineNotFoundException {
 		// TODO Auto-generated method stub
 		log.info("Start");
 		MedicineStock medicineStock = null;
 		log.info("Medicine : {}", medicineDemand);
+		try {
+			medicineStock = stockFeignClient.getNumberOfTabletsInStockByName(token, medicineDemand.getMedicineName());
+		} catch (FeignException ex) {
+			throw new MedicineNotFoundException("Medicine not found");
+		}
 
 		if (medicineStock == null) {
 			throw new MedicineNotFoundException("Medicine not found");
